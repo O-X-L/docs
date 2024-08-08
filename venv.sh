d70648355c3b4d110c36f563a852628f66c9dfe5
Rath Rene <rath@oxl.at>
2024-08-08 22:47:37 +0200

    update venv script

--- venv.sh
+++ venv.sh
@@ -2,7 +2,11 @@
 
 set -euo pipefail
 
+cd "$(dirname "$0")"
+
 VENV_PATH='/tmp/.oxl-docs-venv'
 
 python3 -m virtualenv "$VENV_PATH"
 source "${VENV_PATH}/bin/activate"
+
+pip install -r requirements.txt
